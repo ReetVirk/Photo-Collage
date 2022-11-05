dc2b5cdb9b1d97fee09dd047fe2b08d7563f2f3a
ReetVirk <85968565+ReetVirk@users.noreply.github.com>
2022-11-05 13:10:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start(){
+
+    recognition.start();
+
+}
+
+recognition.onresult = function(event){
+
+    console.log(event);
+    
+    var content = event.results[0][0].transcript;
+    console.log(content);
+
+    if (content == "selfie") {
+
+        console.log("Taking Selfie ---")
+        speak();
+
+    }
+
+};
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    setTimeout(function(){
+
+        img_id = "selfie1";
+        speak_data = "Taking next selfie in 5 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+        take_snapshot();
+
+    }, 5000);
+
+    setTimeout(function(){
+
+        img_id = "selfie2";
+        speak_data = "Taking next selfie in 5 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+        take_snapshot();
+
+    }, 10000);
+
+    setTimeout(function() {
+
+        img_id = "selfie3";
+        take_snapshot();
+
+    }, 15000);
+
+}
+
+function take_snapshot(){
+
+    console.log(img_id);
+
+    Webcam.snap(function(data_uri) {
+
+        if(img_id=="selfie1"){
+
+            document.getElementById("result1").innerHTML = '<img id = "selfie1" src = "'+data_uri+'"/>'
+
+        }
+
+        if(img_id=="selfie2"){
+
+            document.getElementById("result2").innerHTML = '<img id = "selfie2" src = "'+data_uri+'"/>'
+
+        }
+
+        if(img_id=="selfie3"){
+
+            document.getElementById("result3").innerHTML = '<img id = "selfie3" src = "'+data_uri+'"/>'
+
+        }
+
+    });
+
+}
